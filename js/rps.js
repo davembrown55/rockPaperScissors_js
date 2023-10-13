5917391dd48cd56738c57a9e2cc07c88d025ad3c
Dave Brown <davembrown55@googlemail.com>
2023-10-13 17:24:01 +0100

    game now playable in browser. Javascript used to hide / display divs. Message and score updated in browser. User can start round by clickng an html button

--- js/rps.js
+++ js/rps.js
@@ -1,42 +1,119 @@
-// function to play game
-function playRPS () {
+// Play game:
 
 let userScore = 0;
 let compScore = 0;
-let userChoice;
-let compChoice;
 
-while (userScore < 5 && compScore < 5) {
-    userChoice = getInput();
+function playRound(userChoice) {
+
+
     compChoice = getComputerChoice();
 
-    let result = playRound(userChoice, compChoice);
-    if (userScore < 5 && compScore < 5) {
-        alert(result + "\n" + printScore());
-    }
-}
+    let result = getResult(userChoice, compChoice) + "<br/>" + printChoices(userChoice);
 
-alert(result());
+    if (userScore < 5 && compScore < 5) {
+    document.querySelector('.message').innerHTML = result;
+    }
+
+    document.getElementById('userScore').textContent = userScore;
+    document.getElementById('compScore').textContent = compScore;
+
+    if (userScore >= 5) {
+        result = "You Win!";
+        document.querySelector('.message').innerHTML = result;
+        resetGame();
+    } else if (compScore >= 5) {
+        result = "Computer Wins!";
+        document.querySelector('.message').innerHTML = result;
+        resetGame();
+    }
 }
 
 
 //.........Other Functions
 
 function getComputerChoice () {
-    let a = "Rock";
-    let b = "Paper";
-    let c = "Scissors";
+    const choices = ["Rock", "Paper", "Scissors"]; 
+        return choices[Math.floor(Math.random() * choices.length)]
+}
 
-    let x = Math.floor((Math.random() * 100) + 1);
-
-    if (x > 0 && x <= 33) {
-        return a;
-    } else if ( x > 33 && x <= 66 ) {
-        return b;
-    } else if ( x > 66 && x <= 100 ) {
-        return c;
+function getResult (userChoice, compChoice) {
+    switch (true) {
+        case (userChoice === compChoice):
+            return "Draw, No Winner this time!";            
+        case (userChoice === "Rock" && compChoice === "Paper"):
+            compScore++;
+            return "You Lose! Paper beats Rock!"; 
+        case (userChoice === "Rock" && compChoice === "Scissors"):
+            userScore++;
+            return "You Win! Rock beats Scissors!";
+        case (userChoice === "Paper" && compChoice === "Rock"):
+            userScore++;
+            return "You Win, Paper beats Rock!";
+        case (userChoice === "Paper" && compChoice === "Scissors"):
+            compScore++;
+            return "You Lose! Scissors beat Paper!";
+        case (userChoice === "Scissors" && compChoice === "Rock"):
+            compScore++;
+            return "You Lose, Rock beats Scissors!";       
+        case (userChoice === "Scissors" && compChoice === "Paper"):
+            userScore++;
+            return "You Win! Scissors beat paper";
     }
 }
+function printChoices (userChoice) {
+    if (userChoice === compChoice) {
+        return `You both chose ${userChoice}.`;
+    } else {
+    return `The computer chose ${compChoice}. You chose ${userChoice}.`;
+    }
+}
+
+function resetGame(){
+    userScore = 0;
+    compScore = 0;
+    document.getElementById('userScore').textContent = userScore;
+    document.getElementById('compScore').textContent = compScore;
+}
+
+
+// layout 
+// Start game. Button Eventlistener to display gameConent area and hide startContent area.
+
+const startButton = document.querySelector('.startButton');
+
+startButton.addEventListener('click', () => {
+    const gameContent = document.querySelector('.gameContent');
+    gameContent.style.display = 'flex';
+    
+    const startContent = document.querySelector('.startContent');
+    startContent.style.display = 'none';
+
+    let startMessage = "First to 5 wins";
+    document.querySelector('.message').innerHTML = startMessage;
+
+});
+
+// Exit game. Button Eventlistener to display startContent area and hide gameContent area.
+// resets scores and clears message area. 
+
+const exitGame = document.querySelector('.doneBtn');
+
+exitGame.addEventListener('click', () => {
+    const gameContent = document.querySelector('.gameContent');
+    gameContent.style.display = 'none';
+    
+    const startContent = document.querySelector('.startContent');
+    startContent.style.display = 'flex';
+
+    resetGame();
+
+    let startMessage = "";
+    document.querySelector('.message').innerHTML = startMessage;
+
+});
+
+
+// Not needed anymore. The following functions are from when the game was played using prompts and the console.
 
 function getInput() {
     
@@ -55,44 +132,17 @@ function getInput() {
 }
 
 function capitalise (str) {
-    let restOfStr = str.substr(1).toLowerCase()
+    let restOfStr = str.substr(1).toLowerCase();
     let cap = str.charAt(0).toUpperCase();
     return cap + restOfStr;
 }
 
 
-function playRound (userChoice, compChoice) {
-    switch (true) {
-        case (userChoice === "Rock" && compChoice === "Rock"):            
-            return "Draw, No Winner this time!";                
-        case (userChoice === "Rock" && compChoice === "Paper"):
-            compScore++;
-            return "You Lose! Paper beats Rock!"; 
-        case (userChoice === "Rock" && compChoice === "Scissors"):
-            userScore++;
-            return "You Win! Rock beats Scissors!";
-        case (userChoice === "Paper" && compChoice === "Rock"):
-            userScore++;
-            return "You Win, Paper beats Rock!";
-        case (userChoice === "Paper" && compChoice === "Paper"):
-            return "Draw, No Winner this time!";
-        case (userChoice === "Paper" && compChoice === "Scissors"):
-            compScore++;
-            return "You Lose! Scissors beat Paper!";
-        case (userChoice === "Scissors" && compChoice === "Rock"):
-            compScore++;
-            return "You Lose, Rock beats Scissors!";       
-        case (userChoice === "Scissors" && compChoice === "Paper"):
-            userScore++;
-            return "You Win! Scissors beat paper";
-        case (userChoice === "Scissors" && compChoice === "Scissors"):
-            return "Draw, No Winner this time!";
-    }
-}
 function printScore () {
     return `The computer chose ${compChoice} and you chose ${capitalise(userChoice)}.
-Current scores are:\n You: ${userScore}   Computer ${compScore}`
+Current scores are:\n You: ${userScore}   Computer ${compScore}`;
 }
+
 function result () {
     if (userScore === 5) {
         return `You Win! The computer chose ${compChoice} and you chose ${capitalise(userChoice)}.
@@ -104,41 +154,3 @@ Final scores are:\n You: ${userScore}   Computer ${compScore}`
 
 }
 
-// layout 
-
-// Event listener: Click button to change display gameContent and start game
-
-// const startButton = document.querySelector("startButton");
-// startButton.addEventListener('click', )
-
-
-// round using nested if statments in a switch
-    // switch (capitalise(userChoice)) {
-    //     case "Rock":
-    //         if (compChoice === "Rock") {
-    //             console.log("Draw, No Winner this time!");
-    //         } else if (compChoice === "Paper") {
-    //             console.log("You Lose! Paper beats Rock!");
-    //         } else if (compChoice === "Scissors") {
-    //             console.log("You Win! Rock beats Scissors!");
-    //         }    
-    //         break;
-    //     case "Paper":
-    //         if (compChoice === "Rock") {
-    //             console.log("You Win, Paper beats Rock!");
-    //         } else if (compChoice === "Paper") {
-    //             console.log("Draw, No Winner this time!");
-    //         } else if (compChoice === "Scissors") {
-    //             console.log("You Lose! Scissors beat Paper!");
-    //         }  
-    //         break;
-    //     case "Scissors":
-    //         if (compChoice === "Rock") {
-    //             console.log("You Lose, Rock beats Scissors!");
-    //         } else if (compChoice === "Paper") {
-    //             console.log("You Win! Scissors beat paper");
-    //         } else if (compChoice === "Scissors") {
-    //             console.log("Draw, No Winner this time!!");
-    //         } 
-    //         break;
-    // } 
